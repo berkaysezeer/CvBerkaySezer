c9f1e7f9129d58f023b59c0f27570d21eca5a5ac
Berkay Sezer <berkaysezeer@gmail.com>
2023-07-30 23:28:26 +0300

    ana sayfa

--- CvBerkaySezer/Areas/Admin/Controllers/HomeController.cs
+++ CvBerkaySezer/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CvBerkaySezer.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,18 @@ namespace CvBerkaySezer.Areas.Admin.Controllers
         // GET: Admin/Home
         public ActionResult Index()
         {
+            ServiceRepository service = new ServiceRepository();
+            ViewBag.Service = service.List(x => x.IsDeleted == false).Count;
+
+            SkillRepository skill = new SkillRepository();
+            ViewBag.Skill = skill.List(x => x.IsDeleted == false).Count;
+
+            ProjectRepository project = new ProjectRepository();
+            ViewBag.Project = project.List(x => x.IsDeleted == false).Count;
+
+            ContactRepository message = new ContactRepository();
+            ViewBag.Message = message.List(x => x.IsDelete == false && x.IsRead == false).Count;
+
             return View();
         }
     }
